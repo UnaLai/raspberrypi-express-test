7af366729bb7e8c138db7ae04b97427af455ebd1
UnaLai <qqq323618@gmail.com>
2020-03-19 16:32:11 +0800

    [feat]:add env file

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
 const triggerGPIORoute = require('./routes/triggerGPIO')
+const envRoute = require('./routes/env')
+
+require('dotenv').config()
 
 // 解析 body
 app.use(express.json())
+
+// 引入 router
 app.use(triggerGPIORoute)
+app.use(envRoute)
+
 // app.use((req,res,next)=>{
 // 	res.status(404).send('<h1>Page not found</h1>')
 // })
@@ -12,6 +19,8 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+
 app.listen(3000, function () {
   console.log('app listening on port 3000! HIHIHI');
+  console.log(process.env.SERVER_HOST);
 });
